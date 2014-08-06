31e94648f38584e5e94d375872aaece1bbdf30f7
zhao.chao <zhao.chao@immomo.com>
2014-08-06 14:37:43 +0800

    new file:   main/flume-log.go

---
+++ main/flume-log.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"flume-log-sdk/config"
+	"flume-log-sdk/consumer"
+	"log"
+	"os/signal"
+	"runtime"
+	"strings"
+	"syscall"
+)
+
+func main() {
+
+	runtime.GOMAXPROCS(4)
+
+	queuename := flag.String("queuename", "user-log", "config queuename ")
+	redisHost := flag.String("redis", "redis_node_6008:6008", "redishost")
+	maxconn := 10
+	maxIdelTime := 5
+
+	zkhost := flag.String("zkhost", "momo-zk-001.m6:2210,momo-zk-002.m6:2210,momo-zk-003.m6:2210", "zkhost")
+	business := flag.String("businesses", "location", " businesses")
+	flag.Parse()
+
+	log.Printf("queuename:%s,redis:%s,flume:%s\n", *queuename, *redisHost, *zkhost)
+	queueHosts := make([]config.QueueHostPort, 0)
+	for _, hp := range parseHostPort(*redisHost) {
+		qhost := config.QueueHostPort{QueueName: *queuename, Maxconn: maxconn, Timeout: maxIdelTime}
+		qhost.HostPort = hp
+		queueHosts = append(queueHosts, qhost)
+	}
+
+	businessArr := strings.Split(*business, ",")
+	option := config.NewOption(businessArr, *zkhost, queueHosts)
+	sinkmanager := consumer.NewSinkManager(option)
+
+	sinkmanager.Start()
+
+	// reload server list
+	var s = make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGKILL)
+	//是否收到kill的命令
+	<-s
+	sinkmanager.Close()
+
+}
+
+func parseHostPort(hps string) []config.HostPort {
+	hostports := make([]config.HostPort, 0)
+	for _, v := range strings.Split(hps, ",") {
+		hostports = append(hostports, config.NewHostPort(v))
+	}
+
+	return hostports
+}
