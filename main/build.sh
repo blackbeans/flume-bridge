764f79c57901fed64565c33e997cee2aa5080324
zhao.chao <zhao.chao@immomo.com>
2014-08-06 18:50:37 +0800

    modified:   main/build.sh

--- main/build.sh
+++ main/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 VERSION=`date -d last-day +%Y%m%d`
-go build  -o /home/momobot/flume-bin/flume-log-$VERSION flume-log.go
+go build  -o /home/server/flume-bin/flume-log-$VERSION flume-log.go
