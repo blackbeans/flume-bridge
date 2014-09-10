b665c3bae81fac231d2cdfe57225883f25c402cd
zhao.chao <zhao.chao@immomo.com>
2014-09-10 19:41:05 +0800

    增加对redis lpop的监控日志 	new file:   ../consumer/log_monitor.go 	modified:   ../consumer/log_source_manager.go

---
+++ consumer/log_monitor.go
@@ -0,0 +1,59 @@
+package consumer
+
+import (
+	"flume-log-sdk/config"
+	"fmt"
+	"log"
+	"sort"
+	"strconv"
+	"time"
+)
+
+func (self *SourceManager) monitorFlume() {
+	for self.isRunning {
+		time.Sleep(1 * time.Second)
+		monitor := "FLUME_TPS|"
+		for k, v := range self.sourceServers {
+
+			succ, fail := v.monitor()
+			monitor += fmt.Sprintf("%s|%d/%d \t", k, succ, fail)
+		}
+		log.Println(monitor)
+
+		mk := make([]string, 0)
+		monitor = "FLUME_POOL|\n"
+		for k, _ := range self.hp2flumeClientPool {
+			mk = append(mk, k.Host+":"+strconv.Itoa(k.Port))
+		}
+		sort.Strings(mk)
+
+		for _, hp := range mk {
+			v, ok := self.hp2flumeClientPool[config.NewHostPort(hp)]
+			if !ok {
+				continue
+			}
+			active, core, max := v.FlumePool.MonitorPool()
+			monitor += fmt.Sprintf("%s|%d/%d/%d\n", hp, active, core, max)
+		}
+
+		log.Println(monitor)
+		self.monitorRedis()
+	}
+}
+
+func (self *SourceManager) monitorRedis() {
+	for self.isRunning {
+		time.Sleep(1 * time.Second)
+		monitor := "REDIS_TPS|"
+		for k, v := range self.redispool {
+			//队列K
+			monitor += k
+			for _, poolw := range v {
+				cost := (poolw.currValue - poolw.lastValue)
+				poolw.lastValue = poolw.currValue
+				monitor += fmt.Sprintf("|%s|%d \t", poolw.hostport.Host+":"+strconv.Itoa((poolw.hostport.Port)), cost)
+			}
+		}
+		log.Println(monitor)
+	}
+}
--- consumer/log_source_manager.go
+++ consumer/log_source_manager.go
@@ -4,15 +4,23 @@ import (
 	"container/list"
 	"flume-log-sdk/config"
 	"flume-log-sdk/consumer/pool"
-	"fmt"
 	"github.com/blackbeans/redigo/redis"
 	"log"
 	"os"
-	"sort"
 	"strconv"
 	"sync"
 	"time"
 )
+
+type poolwrapper struct {
+	hostport config.HostPort
+
+	rpool *redis.Pool
+
+	lastValue int64
+
+	currValue int64
+}
 
 type SourceManager struct {
 	zkmanager *config.ZKManager
@@ -21,7 +29,7 @@ type SourceManager struct {
 
 	hp2flumeClientPool map[config.HostPort]*pool.FlumePoolLink //对应的Pool
 
-	redisPool map[string][]*redis.Pool // 对应的redispool
+	redispool map[string][]*poolwrapper // 对应的redispool
 
 	watcherPool map[string]*config.Watcher //watcherPool
 
@@ -38,7 +46,7 @@ func NewSourceManager(instancename string, option *config.Option) *SourceManager
 	sourcemanager.sourceServers = make(map[string]*SourceServer)
 	sourcemanager.hp2flumeClientPool = make(map[config.HostPort]*pool.FlumePoolLink)
 	sourcemanager.watcherPool = make(map[string]*config.Watcher)
-	sourcemanager.redisPool = initRedisQueue(option)
+	sourcemanager.redispool = initRedisQueue(option)
 	//从zk中拉取flumenode的配置
 	zkmanager := config.NewZKManager(option.Zkhost)
 	sourcemanager.zkmanager = zkmanager
@@ -49,39 +57,8 @@ func NewSourceManager(instancename string, option *config.Option) *SourceManager
 
 }
 
-func (self *SourceManager) monitorFlume() {
-	for self.isRunning {
-		time.Sleep(1 * time.Second)
-		monitor := "FLUME_TPS|"
-		for k, v := range self.sourceServers {
-
-			succ, fail := v.monitor()
-			monitor += fmt.Sprintf("%s|%d/%d \t", k, succ, fail)
-		}
-		log.Println(monitor)
-
-		mk := make([]string, 0)
-		monitor = "FLUME_POOL|\n"
-		for k, _ := range self.hp2flumeClientPool {
-			mk = append(mk, k.Host+":"+strconv.Itoa(k.Port))
-		}
-		sort.Strings(mk)
-
-		for _, hp := range mk {
-			v, ok := self.hp2flumeClientPool[config.NewHostPort(hp)]
-			if !ok {
-				continue
-			}
-			active, core, max := v.FlumePool.MonitorPool()
-			monitor += fmt.Sprintf("%s|%d/%d/%d\n", hp, active, core, max)
-		}
-
-		log.Println(monitor)
-	}
-}
-
-func initRedisQueue(option *config.Option) map[string][]*redis.Pool {
-	redisPool := make(map[string][]*redis.Pool, 0)
+func initRedisQueue(option *config.Option) map[string][]*poolwrapper {
+	redispool := make(map[string][]*poolwrapper, 0)
 
 	//创建redis的消费连接
 	for _, v := range option.QueueHostPorts {
@@ -96,15 +73,19 @@ func initRedisQueue(option *config.Option) map[string][]*redis.Pool {
 			return
 		}, time.Duration(v.Timeout*2)*time.Second, v.Maxconn/2, v.Maxconn)
 
-		pools, ok := redisPool[v.QueueName]
+		pools, ok := redispool[v.QueueName]
 		if !ok {
-			pools = make([]*redis.Pool, 0)
-			redisPool[v.QueueName] = pools
+			pools = make([]*poolwrapper, 0)
+			redispool[v.QueueName] = pools
 		}
-		redisPool[v.QueueName] = append(pools, pool)
+
+		poolw := &poolwrapper{}
+		poolw.hostport = v.HostPort
+		poolw.rpool = pool
+		redispool[v.QueueName] = append(pools, poolw)
 	}
 
-	return redisPool
+	return redispool
 }
 
 func (self *SourceManager) initSourceServers(businesses []string, zkmanager *config.ZKManager) {
@@ -186,19 +167,20 @@ func (self *SourceManager) Start() {
 	go self.monitorFlume()
 	log.Printf("LOG_SOURCE_MANGER|[%s]|STARTED\n", self.instancename)
 	self.startWorker()
+
 }
 
 func (self *SourceManager) startWorker() {
-	for k, v := range self.redisPool {
 
+	for k, v := range self.redispool {
 		log.Println("LOG_SOURCE|REDIS|[" + k + "]|START")
 		for _, pool := range v {
 
-			go func(queuename string, pool *redis.Pool) {
+			go func(queuename string, pool *poolwrapper) {
 
 				//批量收集数据
-				conn := pool.Get()
-				defer pool.Release(conn)
+				conn := pool.rpool.Get()
+				defer pool.rpool.Release(conn)
 				for self.isRunning {
 
 					reply, err := conn.Do("LPOP", queuename)
@@ -206,13 +188,16 @@ func (self *SourceManager) startWorker() {
 						if nil != err {
 							log.Printf("LPOP|FAIL|%T", err)
 							conn.Close()
-							conn = pool.Get()
+							conn = pool.rpool.Get()
 						} else {
 							time.Sleep(100 * time.Millisecond)
 						}
 
 						continue
 					}
+
+					//计数器++
+					pool.currValue++
 
 					resp := reply.([]byte)
 					businessName, event := decodeCommand(resp)
@@ -243,9 +228,9 @@ func (self *SourceManager) Close() {
 		sourceserver.stop()
 	}
 
-	for _, redispool := range self.redisPool {
+	for _, redispool := range self.redispool {
 		for _, pool := range redispool {
-			pool.Close()
+			pool.rpool.Close()
 		}
 	}
 
